c7ebf0a5b83cd47ee3134408fa3bc1bc22e783bb
Mauricio Fioretti <mauriciofioretti@gmail.com>
2026-04-27 15:53:22 -0300

    Mauri. 27 de Abril 2026. Index. Probando flechitas carrusel

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 const slider = document.querySelector(".novedades_container");
 
 document.getElementById("next").onclick = () => {
-  slider.scrollBy({ left: 270, behavior: "smooth" });
+  slider.scrollBy({ left: 1000, behavior: "smooth" });
 };
 
 document.getElementById("prev").onclick = () => {
-  slider.scrollBy({ left: -270, behavior: "smooth" });
+  slider.scrollBy({ left: -1000, behavior: "smooth" });
 };
